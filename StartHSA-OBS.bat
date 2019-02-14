74d51fd3493e4a19cc41b920947f3f2fa7211fc8
Justin Schweiger <Justin.Schweiger@t-online.de>
2019-02-14 17:33:44 +0100

    implemented update.bat into StartOBS.bat

--- StartHSA-OBS.bat
+++ StartHSA-OBS.bat
@@ -1,3 +1,12 @@
+@echo off
+echo  --------------------------------------------------------
+echo  Checking for Updates ....
+echo  --------------------------------------------------------
+timeout /t 3
+start "" "PortableGit/bin/sh.exe" -c "git fetch --all && git reset --hard origin/master"
+echo  --------------------------------------------------------
+echo  Starte OBS ....
+echo  --------------------------------------------------------
 cd bin/64bit
 start obs64.exe
 exit 0
--- Update.bat
+++
@@ -1,1 +0,0 @@
-start "Update HSA eSports OBS LoL Scenes" "PortableGit/bin/sh.exe" -c "git fetch --all && git reset --hard origin/master"
