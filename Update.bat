a67bbca88118f62af81cc4ce689e6cbeb396da83
Justin Schweiger <40092507+JustinSchweiger@users.noreply.github.com>
2019-02-14 16:55:04 +0100

    Update Update.bat

--- Update.bat
+++ Update.bat
@@ -1,1 +1,1 @@
- start "Update HSA eSports OBS LoL Scenes" "PortableGit/bin/sh.exe" -c "git fetch --all && git reset --hard origin/master"
+start "Update HSA eSports OBS LoL Scenes" "PortableGit/bin/sh.exe" -c "git fetch --all && git reset --hard origin/master"
