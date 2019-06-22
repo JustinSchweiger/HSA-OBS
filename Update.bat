fcf3787a912916b256aab3502c866f3e77eb91db
Justin Schweiger <Justin.Schweiger@t-online.de>
2019-06-23 01:42:20 +0200

    updated update

--- Update.bat
+++ Update.bat
@@ -1,3 +1,5 @@
 timeout /t 2
 start /wait "Update HSA-OBS" "PortableGit/bin/sh.exe" -c "git fetch origin master"
 start /wait "Update HSA-OBS" "PortableGit/bin/sh.exe" -c "git reset --hard origin/master"
+timeout /t 2
+start "" "scripts/deletedirUpdate.bat"
